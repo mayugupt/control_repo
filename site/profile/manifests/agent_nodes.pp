a16fb67f28fe7b5e26ac1cbc2573a65224828778
mayugupt <48587128+mayugupt@users.noreply.github.com>
2020-08-17 18:55:21 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,6 +1,10 @@
 class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm': } 
-  
   dockeragent::node {'db.puppet.vm': } 
+  host { 'web.puppet.vm':
+          ensure => present,
+          ip => '172.18.0.3',
+  }
+          
 }
