c56341fcc44da63a4aa110ac9cd91cbea77d8766
mayugupt <48587128+mayugupt@users.noreply.github.com>
2020-08-17 18:59:25 +0530

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -6,11 +6,11 @@ class profile::ssh_server {
             ensure => 'running',
             enable => 'true', 
     }
-    ssh_authorized_key {'root@master.pupppe.vm':
+    ssh_authorized_key {'root@master.puppet.vm':
             ensure => present,
             user => 'root',
             type => 'ssh-rsa',
-            key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC5w6VH2uz1vdMOQBmMNYRVc386i5CDmrIw5+sdWqeP55Xb9+7TjJpuk2TymCXNHyqvOkfpHkZBxxLygDKQFdFtFjEAP1BWiUn1DFiOu0XFQJnIDP8bVSwSQNqtOlhyF04Teu8jukkv8Chwd8TFuiazkyOQCMxjUt/Nvdv45Vw5pYjqT5WxH644vVxxycYRNPwAtfLLLqq0OZU3G7wKReRsGzKFUwRwNhfqcSKgGsOoC+ib0zsIjB9OGh9xT3X6e3nBEMcEEwttXxeKv9/aU29vGGhsAB+k8BYkdZffB428JRhGudc3gEO6xhs4tdWTCcj50ua9ZVJ1q7OpI7h795xB',
+            key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5w6VH2uz1vdMOQBmMNYRVc386i5CDmrIw5+sdWqeP55Xb9+7TjJpuk2TymCXNHyqvOkfpHkZBxxLygDKQFdFtFjEAP1BWiUn1DFiOu0XFQJnIDP8bVSwSQNqtOlhyF04Teu8jukkv8Chwd8TFuiazkyOQCMxjUt/Nvdv45Vw5pYjqT5WxH644vVxxycYRNPwAtfLLLqq0OZU3G7wKReRsGzKFUwRwNhfqcSKgGsOoC+ib0zsIjB9OGh9xT3X6e3nBEMcEEwttXxeKv9/aU29vGGhsAB+k8BYkdZffB428JRhGudc3gEO6xhs4tdWTCcj50ua9ZVJ1q7OpI7h795xB',
             }
 }
            
