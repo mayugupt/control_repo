7d62a451a500c79060023393aa25f62078a1bfcb
mayugupt <48587128+mayugupt@users.noreply.github.com>
2020-08-17 19:00:36 +0530

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,7 +10,7 @@ class profile::ssh_server {
             ensure => present,
             user => 'root',
             type => 'ssh-rsa',
-            key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5w6VH2uz1vdMOQBmMNYRVc386i5CDmrIw5+sdWqeP55Xb9+7TjJpuk2TymCXNHyqvOkfpHkZBxxLygDKQFdFtFjEAP1BWiUn1DFiOu0XFQJnIDP8bVSwSQNqtOlhyF04Teu8jukkv8Chwd8TFuiazkyOQCMxjUt/Nvdv45Vw5pYjqT5WxH644vVxxycYRNPwAtfLLLqq0OZU3G7wKReRsGzKFUwRwNhfqcSKgGsOoC+ib0zsIjB9OGh9xT3X6e3nBEMcEEwttXxeKv9/aU29vGGhsAB+k8BYkdZffB428JRhGudc3gEO6xhs4tdWTCcj50ua9ZVJ1q7OpI7h795xB',
+            key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC5w6VH2uz1vdMOQBmMNYRVc386i5CDmrIw5+sdWqeP55Xb9+7TjJpuk2TymCXNHyqvOkfpHkZBxxLygDKQFdFtFjEAP1BWiUn1DFiOu0XFQJnIDP8bVSwSQNqtOlhyF04Teu8jukkv8Chwd8TFuiazkyOQCMxjUt/Nvdv45Vw5pYjqT5WxH644vVxxycYRNPwAtfLLLqq0OZU3G7wKReRsGzKFUwRwNhfqcSKgGsOoC+ib0zsIjB9OGh9xT3X6e3nBEMcEEwttXxeKv9/aU29vGGhsAB+k8BYkdZffB428JRhGudc3gEO6xhs4tdWTCcj50ua9ZVJ1q7OpI7h795xB',
             }
 }
            
